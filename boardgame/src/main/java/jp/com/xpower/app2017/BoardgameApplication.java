7a1fd65a1ebeb522275886c43c1cdfa628d656bd
itsuki-oosawa <itsuki-oosawa@x-power.co.jp>
2017-05-18 17:31:34 +0900

    new project

---
+++ boardgame/src/main/java/jp/com/xpower/app2017/BoardgameApplication.java
@@ -0,0 +1,12 @@
+package jp.com.xpower.app2017;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardgameApplication.class, args);
+	}
+}
---
+++ boardgame/src/main/java/jp/com/xpower/app2017/controller/LoginController.java
@@ -0,0 +1,58 @@
+package jp.com.xpower.app2017.controller;
+
+import java.util.Base64;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.com.xpower.app2017.model.RoomCreate;
+import jp.com.xpower.app2017.model.ErrorMessage;
+import jp.com.xpower.app2017.model.SelectUserTable;
+import jp.com.xpower.app2017.model.UserTable;
+
+
+@Controller
+public class LoginController {
+	@Autowired
+	SelectUserTable userTable;
+	@Autowired
+	RoomCreate roomTable;
+	@Autowired
+	ErrorMessage errorMessage;
+	@RequestMapping("/menu")
+    public String menu(HttpSession session,Model model){
+    	boolean isDebug = java.lang.management.ManagementFactory.getRuntimeMXBean().
+    		    getInputArguments().toString().indexOf("-agentlib:jdwp") > 0;
+    	//セッションにuserIdを保存
+    	String userId;
+    	//デバッグモードかどうか判定
+    	//デバッグの場合、セッションスコープに保存
+    	if(isDebug){
+	    	userId = "bbbb";
+	    	session.setAttribute("userId", userId);
+	    	try{
+	    		UserTable selectUserTable = userTable.selectUserTable(userId);
+	    		//ニックネームを取得
+	        	String nickname=selectUserTable.getNickname();
+	        	//画像パスを取得
+	        	//プロフィール画像をHTML上で表示するための、バイナリデータのBase64形式でのエンコード
+		        String encoded = Base64.getEncoder() .encodeToString(selectUserTable.getProfileImage());
+	        	//リクエストスコープに保存して送る
+	        	model.addAttribute("nickname",nickname);
+	        	model.addAttribute("image",encoded);
+	            return "4_menu";
+	    	}catch(Exception e){
+	    		System.out.println(e);
+	    		model.addAttribute("errorMessage",errorMessage.databaseError());
+	    		return "error";
+	    	}
+	    //デバッグでない場合、セッションスコープから取得
+    	}else{
+    		return "4_menu";
+    	}
+    }
+}
