7db0701dec312d9545025990157e5d712fe756e6
itsuki-oosawa <itsuki-oosawa@x-power.co.jp>
2017-05-22 10:44:17 +0900

    メニューです（修正して、まだ動作できない）

--- boardgame/src/main/java/jp/com/xpower/app2017/controller/MenuController.java
+++ boardgame/src/main/java/jp/com/xpower/app2017/controller/MenuController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import jp.com.xpower.app2017.model.ErrorMessage;
 import jp.com.xpower.app2017.model.RoomCreate;
 
 
@@ -16,44 +15,26 @@ import jp.com.xpower.app2017.model.RoomCreate;
 public class MenuController {
 	@Autowired
 	RoomCreate roomTable;
-	@Autowired
-	ErrorMessage errorMessage;
 
 	//ログアウトが押された時の処理
-    @RequestMapping("logout")
-    public String logout(HttpSession session) {
+    @RequestMapping("/logout")//5月19日　メンバー間で命名規則再確認、変更
+    public String requestLogout(HttpSession session) {
     	session.invalidate();//セッション破棄
         return "1_login";
     }
 
-    @RequestMapping("user_profile")
-    public String userProfile(HttpSession session) {
-    	session.invalidate();//セッション破棄
+    @RequestMapping("/userProfile")//5月19日　メンバー間で命名規則再確認、変更
+    public String requestUserProfile() {
         return "3_user_profile";
     }
 
     @RequestMapping("/othello")
-    public String othello(HttpSession session,Model model) {
+    public String requestOthello(HttpSession session,Model model) {
 //    	int i;
 //    	Integer ref_i;
-
-    	//セッションスコープからルームIDを取得
-//    	Object sessionRoomId = session.getAttribute("roomId");
-    	Integer sessionRoomId = (Integer) session.getAttribute("roomId");
-
-    	//セッションスコープからユーザーIDを取得
-    	String userId = (String) session.getAttribute("userId");
-    	//セッションが切れてた場合
-    	if(userId == null){
-    		model.addAttribute("errorMessage",errorMessage.sessionError());
-    		return "error";
-    	}
-    	//セッションにルームIDがない場合
-    	if(sessionRoomId == null){
-    		//ルームIDを取得
-	    	int roomId = roomTable.createRoom(userId);
-	    	//ルームIDをセッションスコープに保存
-	    	session.setAttribute("roomId", roomId);
+    	boolean isError=roomTable.createRoom(session,model);
+    	if(isError==true){
+    		return "/error";
     	}
         return "5_othello";
     }
