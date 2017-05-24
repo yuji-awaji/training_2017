9a2c1cc3eb461cba37a695b72e98f03010819ebd
itsuki-oosawa <itsuki-oosawa@x-power.co.jp>
2017-05-24 14:11:03 +0900

    関数追加しました。

--- boardgame/src/main/java/jp/com/xpower/app2017/model/RoomTable.java
+++ boardgame/src/main/java/jp/com/xpower/app2017/model/RoomTable.java
@@ -96,4 +96,79 @@ public class RoomTable{
 	public void setBattleStartTime(Date battleStartTime) {
 		this.battleStartTime = battleStartTime;
 	}
+
+	//部屋を作成する際の値をセットする
+	public RoomTable create(String userId,String gameId){
+		RoomTable createdRoom = new RoomTable();
+		createdRoom.setRoomMasterId(userId);
+		createdRoom.setGameId(gameId);
+		return createdRoom;
+	}
+
+	//
+	/*public void updateRoomCancel(){
+		//String userId = (String) session.getAttribute("userId");
+    	//セッションが切れてた場合
+    	if(userId == null){
+    		session.invalidate();//セッション破棄
+    		model.addAttribute("errorMessage",Error.SESSIONERROR);
+    	}
+
+    	//セッションスコープからルームIDを取得
+//    	Object sessionRoomId = session.getAttribute("roomId");5月18日指摘　Integer型に変更
+    	RoomTable room = (RoomTable) session.getAttribute("room");
+    	//セッションにルームIDがない場合
+    	if(room == null){
+	    	//userIdがroom_master_idと一致かつroom_stateが対戦待機状態のレコードがあるか取得
+	    	List<RoomTable> cancelList = roomTableRepository.findByRoomMasterIdAndRoomState(userId,State.WAIT);
+	    	//あるのなら、そのレコードのroom_stateを対戦終了状態に変える
+		    if(cancelList.isEmpty() == false){
+		    	RoomTable roomTable = cancelList.get(0);
+		    	updateState(roomTable);
+		    }
+    	}
+	}
+
+
+
+	public void create(HttpSession session,Model model){
+//    	int i;
+//    	Integer ref_i;
+
+    	//セッションスコープからユーザーIDを取得 5月18日指摘　位置移動
+    	String userId = (String) session.getAttribute("userId");
+    	//セッションが切れてた場合
+    	if(userId == null){
+    		session.invalidate();//セッション破棄
+    		model.addAttribute("errorMessage",Error.SESSIONERROR);
+
+    	}
+
+    	//セッションスコープからルームIDを取得
+//    	Object sessionRoomId = session.getAttribute("roomId");5月18日指摘　Integer型に変更
+    	RoomTable room = (RoomTable) session.getAttribute("room");
+    	//セッションにルームIDがない場合
+    	if(room == null){
+	    	//userIdがroom_master_idと一致かつroom_stateが対戦待機状態のレコードがあるか取得
+	    	List<RoomTable> cancelList = roomTableRepository.findByRoomMasterIdAndRoomState(userId,State.WAIT);
+	    	//あるのなら、そのレコードのroom_stateを対戦終了状態に変える
+		    if(cancelList.isEmpty() == false){
+		    	RoomTable roomTable = cancelList.get(0);
+		    	updateState(roomTable);
+		    }
+		    //challenger_idがnullのものかつ、room_stateが対戦待機状態かつroom_masster_idとuserIdが一致しないものを取得
+		    List<RoomTable> selectList = roomTableRepository.findByChallengerIdIsNullAndRoomStateAndRoomMasterIdNot(State.WAIT,userId);
+	    	//取得できなかった場合
+	    	if(selectList.isEmpty()){
+	    		//roomテーブルに、userIDがroom_master_id、game_idが"othello"のレコードを挿入
+	    		room=insertMaster(userId);
+	    	//nullじゃない場合
+	    	}else{
+	    		//roomテーブルのレコードを保存
+	    		RoomTable roomTable = selectList.get(0);
+	    		room=updateChallenger(roomTable, userId);
+	    	}
+	    	model.addAttribute("room",room);
+    	}
+    }*/
 }
