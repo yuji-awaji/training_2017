bead46a354651cb9b84c3fbb905809638acdfecb
itsuki-oosawa <itsuki-oosawa@x-power.co.jp>
2017-05-24 17:36:46 +0900

    5月24日レビュー終了

--- boardgame/src/main/java/jp/com/xpower/app2017/controller/MenuController.java
+++ boardgame/src/main/java/jp/com/xpower/app2017/controller/MenuController.java
@@ -20,8 +20,9 @@ import jp.com.xpower.app2017.model.RoomTableRepository;
 public class MenuController {
 	@Autowired
 	RoomTableRepository roomTableRepository;
-    /*工事中
-    @RequestMapping("/userProfile")//5月19日　メンバー間で命名規則再確認、変更
+
+	/*工事中
+    @RequestMapping("/userProfile")//5月19日 メンバー間で命名規則再確認、変更
     public String userProfile() {
         return "3_user_profile";
     }*/
@@ -34,53 +35,72 @@ public class MenuController {
     	if(isError==true){
     		return "/error";
     	}*/
+    	try{
+	    	//ログイン確認
+	    	String userId = (String) session.getAttribute("userId");
 
-    	//ログイン確認
-    	String userId = (String) session.getAttribute("userId");
-    	System.out.println(userId+"ユーザーID");
-    	if(userId == null){
+	    	if(userId == null){
+	    		session.invalidate();//セッション破棄
+	    		model.addAttribute("errorMessage",Error.SESSIONERROR);
+	    		return "/error";
+	    	}
+
+	    	//セッションにルームがあるか確認
+	    	RoomTable room = (RoomTable) session.getAttribute("room");
+	    	if(room != null){
+	    		return "5_othello";
+	    	}
+
+	    	//ログインしているユーザーがmasterで部屋状態が待機中の部屋を探索
+	    	List<RoomTable> invalidRooms = roomTableRepository.findByRoomMasterIdAndRoomState(userId,State.WAIT);
+
+	    	//不要な部屋を対戦終了状態にする 5月24日レビューにてコメント変更 いらない→不要な
+	    	for(RoomTable invalidRoom:invalidRooms){
+	    		invalidRoom.setRoomState(State.END);
+	    		roomTableRepository.save(invalidRoom);
+	    	}
+
+	    	//チャレンジャーが入れる部屋があるかどうかを調べる
+	    	//5月24日レビューにて変数名修正 challengebleRooms→challengeableRooms
+	    	List<RoomTable> challengeableRooms = roomTableRepository.findByChallengerIdIsNullAndRoomStateAndRoomMasterIdNot(State.WAIT,userId);
+	    	if(challengeableRooms.isEmpty()){
+	    		//部屋のレコードを新たに作る 5月24日のレビューにて = の両隣が詰まっていたので空けた
+	    		RoomTable createdRoom = RoomTable.create(userId, "othello");
+	    		roomTableRepository.save(createdRoom);
+	    		session.setAttribute("room", createdRoom);
+	    		//boolean isMasterFlag = true; 5月24日のレビューにて修正
+	    		session.setAttribute("isMasterFlag", true);
+
+	    		/*5月24日レビューにて修正 isMasterFlagに変更してマスターかどうかを判定
+	    		 * session.setAttribute("roomMasterNickname",(String)session.getAttribute("nickname"));
+	    		session.setAttribute("roomMasterImage",(String)session.getAttribute("image"));*/
+	    	}else{
+	    		//部屋にチャレンジャーIDを入れて、対戦開始状態にする。
+
+	    		//5月24日レビューにて修正 変数名をtergetRoomからchallengeableRoomに変更
+	    		//RoomTable tergetRoom = challengebleRooms.get(0);
+	    		RoomTable challengeableRoom = challengeableRooms.get(0);
+	    		//5月24日レビューにて修正 変数名をtergetRoomからchallengeableRoomに変更
+	    		challengeableRoom.setChallengerId(userId);
+	    		//5月24日レビューにて修正 変数名をtergetRoomからchallengeableRoomに変更
+	    		challengeableRoom.setRoomState(State.MIDST);
+	    		Date currentTime = new Date();
+	    		//5月24日レビューにて修正 変数名をtergetRoomからchallengeableRoomに変更
+	    		challengeableRoom.setBattleStartTime(currentTime);
+	    		roomTableRepository.save(challengeableRoom);
+	    		//5月24日レビューにて修正 変数名をtergetRoomからchallengeableRoomに変更
+	    		session.setAttribute("room", challengeableRoom);
+	    		//boolean isMasterFlag = false;
+	    		session.setAttribute("isMasterFlag", false);
+	    		/*5月24日レビューにて指摘 isMasterFlagに変更してマスターかどうかを判定
+	    		 * session.setAttribute("challengerNickname",(String)session.getAttribute("nickname"));
+	    		session.setAttribute("challengerImage",(String)session.getAttribute("image"));*/
+	    	}
+    	}catch(Exception e){
     		session.invalidate();//セッション破棄
-    		model.addAttribute("errorMessage",Error.SESSIONERROR);
+    		model.addAttribute("errorMessage",Error.DATABASEERROR);
     		return "/error";
     	}
-
-    	//セッションにルームがあるか確認
-    	RoomTable room = (RoomTable) session.getAttribute("room");
-    	if(room != null){
-    		return "5_othello";
-    	}
-
-    	//ログインしているユーザーがmasterで部屋状態が待機中の部屋を探索
-    	List<RoomTable> invalidRooms = roomTableRepository.findByRoomMasterIdAndRoomState(userId,State.WAIT);
-
-    	//いらない部屋を対戦終了状態にする
-    	for(RoomTable invalidRoom:invalidRooms){
-    		invalidRoom.setRoomState(State.END);
-    		roomTableRepository.save(invalidRoom);
-    	}
-    	//チャレンジャーが入れる部屋があるかどうかを調べる
-    	List<RoomTable> challengebleRooms = roomTableRepository.findByChallengerIdIsNullAndRoomStateAndRoomMasterIdNot(State.WAIT,userId);
-    	if(challengebleRooms.isEmpty()){
-    		//部屋のレコードを新たに作る
-    		RoomTable createdRoom = new RoomTable();
-    		createdRoom=createdRoom.create(userId, "othello");
-    		roomTableRepository.save(createdRoom);
-    		session.setAttribute("room", createdRoom);
-    		session.setAttribute("roomMasterNickname",(String)session.getAttribute("nickname"));
-    		session.setAttribute("roomMasterImage",(String)session.getAttribute("image"));
-    	}else{
-    		//部屋にチャレンジャーIDを入れて、対戦開始状態にする。
-    		RoomTable targetRoom = challengebleRooms.get(0);
-    		targetRoom.setChallengerId(userId);
-    		targetRoom.setRoomState(State.MIDST);
-    		Date currentTime = new Date();
-    		targetRoom.setBattleStartTime(currentTime);
-    		roomTableRepository.save(targetRoom);
-    		session.setAttribute("room", targetRoom);
-    		session.setAttribute("challengerNickname",(String)session.getAttribute("nickname"));
-    		session.setAttribute("challengerImage",(String)session.getAttribute("image"));
-    	}
-
         return "5_othello";
     }
 
